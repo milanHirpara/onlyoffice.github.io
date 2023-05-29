c0c0918300fc5d30e602dfab220b809b87d4122e
Milan Hirpara <milan.encodedots@gmail.com>
2023-05-29 12:28:52 +0530

    Console Test

--- sdkjs-plugins/content/propact/scripts/pluginCode.js
+++ sdkjs-plugins/content/propact/scripts/pluginCode.js
@@ -3,6 +3,9 @@
     var text = "Hello this is propact plugin testing!11";
 
     window.Asc.plugin.init = function () {
+        document.addEventListener('mousemove', function (e) {
+            console.log("MOUSEMOVE", e)
+        })
         var variant = 2;
         switch (variant) {
             case 0: {
@@ -29,21 +32,22 @@
             }
             case 2: {
 
-                window.Asc.plugin.executeMethod("GetSelectedText", [{Numbering:true}], function(data) {
-                    console.log("GetSelectedTextDATA",data)
+                window.Asc.plugin.executeMethod("GetSelectedText", [{Numbering: true}], function (data) {
+                    console.log("GetSelectedTextDATA", data)
                 });
                 console.log("INSIDE THE CASE 3")
                 var _info = window.Asc.plugin.info;
-                console.log("_info",_info)
-                console.log("_infoDATA",_info.data)
-                console.log("_info.width",_info.width)
-                console.log("_info.height",_info.height)
-                console.log("_info.mmToPx * _info.width",_info.mmToPx * _info.width)
-                console.log("_info.mmToPx * _info.height",_info.mmToPx * _info.height)
+                console.log("_info", _info)
+                console.log("_infoDATA", _info.data)
+                console.log("_info.width", _info.width)
+                console.log("_info.height", _info.height)
+                console.log("_info.mmToPx * _info.width", _info.mmToPx * _info.width)
+                console.log("_info.mmToPx * _info.height", _info.mmToPx * _info.height)
                 // call command with external variables
                 Asc.scope.text = text; // export variable to plugin scope
                 this.callCommand(function () {
                     var oDocument = Api.GetDocument();
+                    console.log("oDocument", oDocument)
                     var oParagraph = Api.CreateParagraph();
                     oParagraph.AddText(Asc.scope.text); // or oParagraph.AddText(scope.text);
                     oDocument.InsertContent([oParagraph]);
